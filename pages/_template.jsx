c02815c4a374b9077e5095253bc59626071ec18e
Ross <ross.ad@gmail.com>
2016-06-05 01:12:21 -0700

    Create links for additional content

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -13,6 +13,12 @@ module.exports = React.createClass({
     }
   },
   render () {
+    const navStyle ={ 
+      color: 'black',
+      float: 'right',
+      textDecoration: 'none',
+      padding: '0.25em'
+    }
     return (
       <div>
         <Headroom
@@ -35,10 +41,17 @@ module.exports = React.createClass({
               style={{
                 color: 'black',
                 textDecoration: 'none',
+                padding:'0.25em'
               }}
             >
               Home
             </Link>
+            <Link to={prefixLink('/contact/')} style={navStyle}>Contact</Link>
+            <Link to={prefixLink('/portfolio/')} style={navStyle}>Portfolio</Link>
+            <Link to={prefixLink('/resume/')} style={navStyle}>Resume</Link>
+            <Link to={prefixLink('/about/')} style={navStyle}>About</Link>
+            <Link to={prefixLink('/blog/')} style={navStyle}>Blog</Link>
+                                                
           </Container>
         </Headroom>
         <Container
